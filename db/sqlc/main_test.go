adb3e411ca1f0dd86bd94e8e573c2d5940ba9a82
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-02-24 00:26:08 +0100

    laod config from anv vars

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,20 +6,19 @@ import (
 	"os"
 	"testing"
 
+	"github.com/Omar-Belghaouti/simple-bank/util"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgres://omar:ramo@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to database:", err)
 	}
--- main.go
+++ main.go
@@ -6,17 +6,16 @@ import (
 
 	api "github.com/Omar-Belghaouti/simple-bank/api"
 	db "github.com/Omar-Belghaouti/simple-bank/db/sqlc"
+	"github.com/Omar-Belghaouti/simple-bank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgres://omar:ramo@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to database:", err)
 	}
@@ -24,7 +23,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
